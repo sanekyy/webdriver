53c30eff5a785ea130b744e4dc3b1bf9641c7d90
sanekyy <sanekyy@gmail.com>
2017-05-23 16:11:37 +0300

    add json report path

--- src/test/java/com/spbstu/webdriver/Runner.java
+++ src/test/java/com/spbstu/webdriver/Runner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/resources/data/mantisbt/features",
-        glue = "com.spbstu.webdriver.stepDefs"
+        glue = "com.spbstu.webdriver.stepDefs",
+        format = "json:target/reports/mainReport.json"
 )
 public class Runner extends BaseTest {
 
